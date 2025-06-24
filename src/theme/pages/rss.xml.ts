c35d976ebd7e4e1e6bd55802f140e40859856c30
miaobuao <miaobuao@outlook.com>
2025-06-24 17:56:16 +0800

    fix: optional chaining for site title and description in RSS feed

--- src/theme/pages/rss.xml.ts
+++ src/theme/pages/rss.xml.ts
@@ -6,8 +6,8 @@ import config from '../consts'
 export const GET: APIRoute = async function (context) {
 	const posts = await getCollection('blog')
 	return rss({
-		title: config.site.title ?? '',
-		description: config.site.description ?? '',
+		title: config?.site?.title ?? '',
+		description: config?.site?.description ?? '',
 		site: context.site!,
 		items: posts.map((post) => ({
 			author: config.author.name,
