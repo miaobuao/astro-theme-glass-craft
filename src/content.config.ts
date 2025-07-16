7494d1262d7ff71837ac0b9276525c246feeb4d3
miaobuao <miaobuao@outlook.com>
2025-07-16 19:06:27 +0800

    feat: add more fields

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,11 +5,25 @@ const blog = defineCollection({
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	schema: ({ image }) =>
 		z.object({
-			title: z.string(),
-			description: z.string(),
+			title: z.string().optional(),
+			description: z.string().optional(),
+			summary: z.string().optional(),
+			tags: z.array(z.string()).optional(),
+
+			// pub date
+			date: z.coerce.date().optional(),
+			ctime: z.coerce.date().optional(),
 			pubDate: z.coerce.date(),
+
+			// mtime
+			mtime: z.coerce.date().optional(),
 			updatedDate: z.coerce.date().optional(),
+
+			// cover
 			heroImage: image().optional(),
+			cover: image().optional(),
+
+			draft: z.boolean().optional().default(false),
 		}),
 })
 
