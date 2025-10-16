e6c177b3a61750fc28c02dfa744d8e50e76c726b
miaobuao <miaobuao@outlook.com>
2025-10-16 14:44:35 +0800

    icon

--- src/theme/components/solid/WindowManager/Window.tsx
+++ src/theme/components/solid/WindowManager/Window.tsx
@@ -1,9 +1,32 @@
 import type { WindowProps } from './emitter'
 
-export function Window(props: WindowProps) {
+export function Window(
+	props: WindowProps & {
+		onResize?: (width: number, height: number) => void
+		onMove?: (x: number, y: number) => void
+	},
+) {
 	return (
-		<div>
-			<iframe src={props.url.toString()}></iframe>
+		<div
+			class="flex flex-col fixed"
+			style={{
+				width: props.geometry.width + 'px',
+				height: props.geometry.height + 'px',
+				left: props.geometry.x + 'px',
+				top: props.geometry.y + 'px',
+			}}
+		>
+			<section class="flex">
+				<span></span>
+				<span>{props.title}</span>
+				<span class="flex gap-1">
+					<i class="icon-[mdi--minimize]"></i>
+					<i class="icon-[mdi--maximize]"></i>
+					<i class="icon-[mdi--close]"></i>
+				</span>
+			</section>
+
+			<iframe class="flex-1" src={props.url.toString()}></iframe>
 		</div>
 	)
 }
